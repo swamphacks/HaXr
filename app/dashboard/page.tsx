fd2c72163d91f0965ea1884f187bb4d1dbc0fc04
Robert Conde <rconde2003@gmail.com>
2024-04-20 20:42:43 -0400

    Remove unneeded conditional

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,20 +2,19 @@
 import { ClerkLoaded, ClerkLoading, useSession } from '@clerk/nextjs';
 
 export default function Subpage() {
-  const { isLoaded, session } = useSession();
+  const { session } = useSession();
 
-  if (isLoaded)
-    return (
-      <>
-        <ClerkLoading>
-          <div>Loading...</div>
-        </ClerkLoading>
-        <ClerkLoaded>
-          <div>
-            Hi, {session?.user.firstName}! You are logged in (this page is
-            hidden to logged out people using middleware).
-          </div>
-        </ClerkLoaded>
-      </>
-    );
+  return (
+    <>
+      <ClerkLoading>
+        <div>Loading...</div>
+      </ClerkLoading>
+      <ClerkLoaded>
+        <div>
+          Hi, {session?.user.firstName}! You are logged in (this page is hidden
+          to logged out people using middleware).
+        </div>
+      </ClerkLoaded>
+    </>
+  );
 }
