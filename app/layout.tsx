9b6b6d07ca7d8c47025ac58d8b62b9f70c28d6f3
Robert Conde <rconde2003@gmail.com>
2024-04-28 20:09:57 -0400

    Layout for admin portal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import type { Metadata } from 'next';
+import '@mantine/core/styles.css';
 import './globals.css';
 import React from 'react';
 import { ClerkProvider } from '@clerk/nextjs';
+import { ColorSchemeScript, MantineProvider } from '@mantine/core';
+import Head from 'next/head';
+import { dark } from '@clerk/themes';
 
 export const metadata: Metadata = {
-  title: 'SwampHacks X',
+  title: 'SwampHacks Portal',
   description: 'SwampHacks portal for Hackers and Admins!',
 };
 
@@ -14,9 +18,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{ baseTheme: dark }}>
       <html lang='en'>
-        <body>{children}</body>
+        <Head>
+          <ColorSchemeScript />
+        </Head>
+        <body>
+          <MantineProvider defaultColorScheme='dark'>
+            {children}
+          </MantineProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
