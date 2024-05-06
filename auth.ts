62944c88e6c921e1d46833629343d8fb9538950e
Robert Conde <rconde2003@gmail.com>
2024-05-06 01:23:42 -0400

    Authorization

---
+++ auth.ts
@@ -0,0 +1,29 @@
+import NextAuth from 'next-auth';
+import GitHub from '@auth/core/providers/github';
+import { PrismaClient } from '@prisma/client';
+import { Pool } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+
+// Edge connection
+const neon = new Pool({
+  connectionString: process.env.AUTH_POSTGRES_PRISMA_URL,
+});
+const adapter = new PrismaNeon(neon);
+const prisma = new PrismaClient({ adapter });
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [GitHub],
+  callbacks: {
+    // authorized({ request, auth }) {
+    //   const { pathname } = request.nextUrl
+    //   if (pathname === "/middleware-example") return !!auth
+    //   return true
+    // },
+    jwt({ token, trigger, session }) {
+      if (trigger === 'update') token.name = session?.user?.name;
+      return token;
+    },
+  },
+});
