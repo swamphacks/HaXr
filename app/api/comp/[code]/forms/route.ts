8bc5afa63e2047d754ca03e75f3178ed85298fbf
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-05-22 21:49:57 -0700

    Remove ListForms.tsx

--- app/api/comp/[code]/forms/route.ts
+++ app/api/comp/[code]/forms/route.ts
@@ -3,14 +3,18 @@ import { PrismaClient } from '@prisma/client';
 import { Pool } from '@neondatabase/serverless';
 import { PrismaNeon } from '@prisma/adapter-neon';
 
-export async function GET(req: NextRequest) {
+export async function GET(req: NextRequest, { code }: { code: string }) {
 	const neon = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL });
 	const adapter = new PrismaNeon(neon);
 	const prisma = new PrismaClient({ adapter });
 
-	const competitions = await prisma.competition.findMany({
+
+	const competitions = await prisma.form.findMany({
+		where: {
+			competition_code: code,
+		},
 		orderBy: {
-			start_date: 'desc', // Most recent first
+			update_at: 'desc', // Most recent first
 		},
 	});
 
