36b8ca942e8413d06961265b1b43d7d9b865b4e9
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-07-25 15:05:57 -0700

    added state lets go

--- types/forms.ts
+++ types/forms.ts
@@ -1,62 +1,65 @@
 import { questionType } from '@/types/questionTypes';
 
 export enum FileType {
-	PDF = 'PDF',
-	IMG = 'Image',
-	VIDEO = 'Video'
+  PDF = 'PDF',
+  IMG = 'Image',
+  VIDEO = 'Video',
 }
 export const fileTypes: FileType[] = [
-	FileType.PDF,
-	FileType.IMG,
-	FileType.VIDEO
-]
+  FileType.PDF,
+  FileType.IMG,
+  FileType.VIDEO,
+];
+
+export const fileSizes = ['1MB', '10MB', '100MB', '1GB'];
 
 export interface Settings {
-	maxChars?: number;
-	acceptedFiles?: FileType[];
-	required: boolean;
+  maxChars?: number;
+  acceptedFiles?: FileType[];
+  maxFileSize?: string;
+  required: boolean;
 }
 
 export interface Question {
-	title: string;
-	description: string;
-	type: questionType;
-	key: string;
-	choices?: string[];
-	settings: Settings;
+  title: string;
+  description: string;
+  type: questionType;
+  key: string;
+  choices?: string[];
+  settings: Settings;
 }
 
 export interface Agreement {
-	label: string;
-	required: boolean;
-	key: string;
+  label: string;
+  required: boolean;
+  key: string;
 }
 
 export interface Section {
-	key: string;
-	title: string;
-	description: string;
-	questions: Question[];
+  key: string;
+  title: string;
+  description: string;
+  questions: Question[];
 }
 
 export interface Application {
-	general: Section;
-	agreements: Agreement[];
-	sections: Record<string, Section>;
+  general: Section;
+  agreements: Agreement[];
+  sections: Record<string, Section>;
 }
 
 export interface Response {
-	key: string;
-	value: string | number | boolean;
+  key: string;
+  value: string | number | boolean;
 }
 
 export interface SectionResponse {
-	key: string;
-	responses: Response[];
+  key: string;
+  responses: Response[];
 }
 
 export interface ApplicationResponse {
-	general: Response[];
-	agreements: Response[];
-	sections: Record<string, Response[]>;
+  general: Response[];
+  agreements: Response[];
+  sections: Record<string, Response[]>;
 }
