0fcf8ec6196523f7d9ba14ff6434adf16d3922cc
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-07-25 10:53:52 -0700

    answer choices baby

--- types/forms.ts
+++ types/forms.ts
@@ -1,44 +1,55 @@
 import { questionType } from '@/types/questionTypes';
 
+export enum FileType {
+	PDF = '.pdf',
+}
+
+export interface Settings {
+	maxChars?: number;
+	acceptedFiles?: FileType[];
+	required: boolean;
+}
+
 export interface Question {
-  title: string;
-  type: questionType;
-  required: boolean;
-  key: string;
-  choices?: string[];
+	title: string;
+	description: string;
+	type: questionType;
+	key: string;
+	choices?: string[];
+	settings: Settings;
 }
 
 export interface Agreement {
-  label: string;
-  required: boolean;
-  key: string;
+	label: string;
+	required: boolean;
+	key: string;
 }
 
 export interface Section {
-  key: string;
-  title: string;
-  description: string;
-  questions: Question[];
+	key: string;
+	title: string;
+	description: string;
+	questions: Question[];
 }
 
 export interface Application {
-  general: Section;
-  agreements: Agreement[];
-  sections: Record<string, Section>;
+	general: Section;
+	agreements: Agreement[];
+	sections: Record<string, Section>;
 }
 
 export interface Response {
-  key: string;
-  value: string | number | boolean;
+	key: string;
+	value: string | number | boolean;
 }
 
 export interface SectionResponse {
-  key: string;
-  responses: Response[];
+	key: string;
+	responses: Response[];
 }
 
 export interface ApplicationResponse {
-  general: Response[];
-  agreements: Response[];
-  sections: Record<string, Response[]>;
+	general: Response[];
+	agreements: Response[];
+	sections: Record<string, Response[]>;
 }
