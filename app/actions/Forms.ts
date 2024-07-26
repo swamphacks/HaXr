c9c378e86031a002092c4638a861cb9233d30a6a
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-07-26 10:14:14 -0700

    renders on forms

--- app/actions/Forms.ts
+++ app/actions/Forms.ts
@@ -1,223 +1,235 @@
 'use server';
 
 import {
-  Prisma,
-  PrismaClient,
-  Form,
-  FormType,
-  FormSettings,
+	Prisma,
+	PrismaClient,
+	Form,
+	FormType,
+	FormSettings,
 } from '@prisma/client';
 import { Pool } from '@neondatabase/serverless';
 import { PrismaNeon } from '@prisma/adapter-neon';
 import { ApplicationResponse, FormSection } from '@/types/forms';
 
 const neon = new Pool({
-  connectionString: process.env.POSTGRES_PRISMA_URL,
+	connectionString: process.env.POSTGRES_PRISMA_URL,
 });
 const adapter = new PrismaNeon(neon);
 const prisma = new PrismaClient({ adapter });
 
+
+export async function getApplication(competition_code: string) {
+	const resp = prisma.competition.findFirst({
+		where: {
+			code: competition_code
+		},
+		include: {
+			application: true,
+		}
+	});
+	return resp;
+}
+
 export async function getForm(formId: string) {
-  const form = await prisma.form.findFirst({
-    where: {
-      id: formId,
-    },
-    include: {
-      settings: true,
-    },
-  });
-  return form;
+	const form = await prisma.form.findFirst({
+		where: {
+			id: formId,
+		},
+		include: {
+			settings: true,
+		},
+	});
+	return form;
 }
 
 export async function createForm(competitionCode: string, formType: FormType) {
-  const settings = await prisma.formSettings.create({
-    data: {},
-  });
+	const settings = await prisma.formSettings.create({
+		data: {},
+	});
 
-  return await prisma.form.create({
-    data: {
-      competition_code: competitionCode,
-      settings_id: settings.id,
-      form_type: formType,
-    },
-  });
+	return await prisma.form.create({
+		data: {
+			competition_code: competitionCode,
+			settings_id: settings.id,
+			form_type: formType,
+		},
+	});
 }
 
 export async function updateForm(
-  form: Form,
-  settings: FormSettings,
-  sections: FormSection[]
+	form: Form,
+	settings: FormSettings,
+	sections: FormSection[]
 ) {
-  const resp = await prisma.form.update({
-    where: {
-      id: form.id,
-    },
-    data: {
-      title: form.title,
-      is_primary: form.is_primary,
-      is_published: form.is_published,
-      sections: sections as unknown as Prisma.JsonArray,
-      settings: {
-        update: settings,
-      },
-    },
-  });
+	const resp = await prisma.form.update({
+		where: {
+			id: form.id,
+		},
+		data: {
+			title: form.title,
+			is_published: form.is_published,
+			sections: sections as unknown as Prisma.JsonArray,
+			settings: {
+				update: settings,
+			},
+		},
+	});
 
-  return resp;
+	return resp;
 }
 
 export async function createResponse(
-  formId: string,
-  userId: string,
-  answers: Record<string, string>
+	formId: string,
+	userId: string,
+	answers: Record<string, string>
 ) {
-  const resp = await prisma.response.create({
-    data: {
-      form_id: formId,
-      submitted_by_id: userId,
-      answers: answers,
-    },
-  });
+	const resp = await prisma.response.create({
+		data: {
+			form_id: formId,
+			submitted_by_id: userId,
+			answers: answers,
+		},
+	});
 
-  return resp;
+	return resp;
 }
 
-export async function getResponse(
-  formId: string,
-  userId: string
+export async function getFormResponse(
+	formId: string,
+	userId: string
 ): Promise<ApplicationResponse> {
-  // let resp = await prisma.response.findFirst({
-  // 	where: {
-  // 		form_id: formId,
-  // 		submitted_by_id: userId,
-  // 	},
-  // });
-  //
-  // // Create a new response if it doesn't exist
-  // if (!resp) {
-  // 	resp = await prisma.response.create({
-  // 		data: {
-  // 			form_id: formId,
-  // 			submitted_by_id: userId,
-  // 			answers: responses,
-  // 		},
-  // 	});
-  // }
-  // return resp;
-  return {
-    general: [
-      {
-        key: '1',
-        value: 'Matthew',
-      },
-      {
-        key: '2',
-        value: 'DeGuzman',
-      },
-      {
-        key: '3',
-        value: '18-24',
-      },
-      {
-        key: '4',
-        value: 'matthew@ufl.edu',
-      },
-      {
-        key: '5',
-        value: 'University of Florida',
-      },
-      {
-        key: '6',
-        value: 'Other',
-      },
-      {
-        key: '7',
-        value: 'Computer Science',
-      },
-      {
-        key: '8',
-        value: 'Florida',
-      },
-      {
-        key: '9',
-        value: 'Yes',
-      },
-      {
-        key: '10',
-        value: 'Man',
-      },
-      {
-        key: '11',
-        value: 'Other',
-      },
-      {
-        key: '12',
-        value: 'White',
-      },
-    ],
-    agreements: [
-      {
-        key: '13',
-        value: true,
-      },
-      {
-        key: '14',
-        value: true,
-      },
-      {
-        key: '15',
-        value: false,
-      },
-    ],
-    sections: {
-      shortAnswer: [
-        {
-          key: '16',
-          value: 'I am excited to hack at this hackathon.',
-        },
-        {
-          key: '17',
-          value: 'I am proud of making this.',
-        },
-        {
-          key: '18',
-          value: 'I would like to make money.',
-        },
-      ],
-    },
-  };
+	// let resp = await prisma.response.findFirst({
+	// 	where: {
+	// 		form_id: formId,
+	// 		submitted_by_id: userId,
+	// 	},
+	// });
+	//
+	// // Create a new response if it doesn't exist
+	// if (!resp) {
+	// 	resp = await prisma.response.create({
+	// 		data: {
+	// 			form_id: formId,
+	// 			submitted_by_id: userId,
+	// 			answers: responses,
+	// 		},
+	// 	});
+	// }
+	// return resp;
+	return {
+		general: [
+			{
+				key: '1',
+				value: 'Matthew',
+			},
+			{
+				key: '2',
+				value: 'DeGuzman',
+			},
+			{
+				key: '3',
+				value: '18-24',
+			},
+			{
+				key: '4',
+				value: 'matthew@ufl.edu',
+			},
+			{
+				key: '5',
+				value: 'University of Florida',
+			},
+			{
+				key: '6',
+				value: 'Other',
+			},
+			{
+				key: '7',
+				value: 'Computer Science',
+			},
+			{
+				key: '8',
+				value: 'Florida',
+			},
+			{
+				key: '9',
+				value: 'Yes',
+			},
+			{
+				key: '10',
+				value: 'Man',
+			},
+			{
+				key: '11',
+				value: 'Other',
+			},
+			{
+				key: '12',
+				value: 'White',
+			},
+		],
+		agreements: [
+			{
+				key: '13',
+				value: true,
+			},
+			{
+				key: '14',
+				value: true,
+			},
+			{
+				key: '15',
+				value: false,
+			},
+		],
+		sections: {
+			shortAnswer: [
+				{
+					key: '16',
+					value: 'I am excited to hack at this hackathon.',
+				},
+				{
+					key: '17',
+					value: 'I am proud of making this.',
+				},
+				{
+					key: '18',
+					value: 'I would like to make money.',
+				},
+			],
+		},
+	};
 }
 
 export async function saveResponse(
-  responseId: string,
-  responses: Record<string, string>
+	responseId: string,
+	responses: Record<string, string>
 ) {
-  const resp = await prisma.response.update({
-    where: {
-      id: responseId,
-      submitted: false,
-    },
-    data: {
-      answers: responses,
-    },
-  });
+	const resp = await prisma.response.update({
+		where: {
+			id: responseId,
+			submitted: false,
+		},
+		data: {
+			answers: responses,
+		},
+	});
 
-  return resp;
+	return resp;
 }
 
 export async function submitResponse(
-  responseId: string,
-  responses: Record<string, string>
+	responseId: string,
+	responses: Record<string, string>
 ) {
-  const resp = await prisma.response.update({
-    where: {
-      id: responseId,
-    },
-    data: {
-      answers: responses,
-      submitted: true,
-    },
-  });
+	const resp = await prisma.response.update({
+		where: {
+			id: responseId,
+		},
+		data: {
+			answers: responses,
+			submitted: true,
+		},
+	});
 
-  return resp;
+	return resp;
 }
---
+++ app/hacker/forms/application/[code]/page.tsx
@@ -0,0 +1,263 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import {
+	TextInput,
+	Textarea,
+	Select,
+	Checkbox,
+	Divider,
+	Stack,
+	Button,
+	Group,
+	Text,
+	Title,
+	Radio,
+	FileButton,
+} from '@mantine/core';
+import { useForm, UseFormReturnType, isEmail, isNotEmpty, } from '@mantine/form';
+import {
+	ageChoices,
+	schools,
+	levelOfStudies,
+	countries,
+	questionType,
+} from '@/types/questionTypes';
+import { PhoneInput } from 'react-international-phone';
+import 'react-international-phone/style.css';
+import { PhoneNumberUtil } from 'google-libphonenumber';
+import { getApplication, getFormResponse, saveResponse, submitResponse } from '@/app/actions/Forms';
+import {
+	Question as QuestionInterface,
+	Agreement,
+	FormSection,
+	Application,
+	ApplicationResponse,
+} from '@/types/forms';
+import { application, mantineForm, MantineForm } from '@/forms/application';
+import { Form, Competition } from '@prisma/client';
+
+function Question({ question, form }: { question: QuestionInterface, form: UseFormReturnType<Record<string, any>> }) {
+	const [file, setFile] = useState<File | null>(null);
+	switch (question.type) {
+		case questionType.shortResponse:
+		case questionType.email:
+			return (
+				<TextInput
+					label={question.title}
+					required={question.settings.required}
+					key={form.key(question.key)}
+					{...form.getInputProps(question.key)}
+				/>
+			);
+		case questionType.paragraph:
+			return (
+				<Textarea
+					label={question.title}
+					required={question.settings.required}
+					resize="vertical"
+					key={form.key(question.key)}
+					{...form.getInputProps(question.key)}
+				/>
+			)
+		case questionType.multiplechoice:
+			return (
+				<Radio.Group
+					label={question.title}
+					required={question.settings.required}
+					key={form.key(question.key)}
+					{...form.getInputProps(question.key)}
+				>
+					<Stack>
+						{question.choices?.map((choice: string, index: number) => {
+							return (
+								<Radio key={index} label={choice} value={choice} />
+							);
+						})}
+					</Stack>
+				</Radio.Group>
+			)
+
+		case questionType.checkbox:
+			return (
+				<Checkbox.Group
+					label={question.title}
+					required={question.settings.required}
+					key={form.key(question.key)}
+					{...form.getInputProps(question.key)}
+				>
+					<Stack>
+						{question.choices?.map((choice: string, index: number) => {
+							return (
+								<Checkbox key={index} label={choice} value={choice} />
+							);
+						})}
+					</Stack>
+				</Checkbox.Group>
+			)
+
+		case questionType.dropdown:
+			return (
+				<Select
+					label={question.title}
+					required={question.settings.required}
+					data={question.choices}
+					key={form.key(question.key)}
+					{...form.getInputProps(question.key)}
+					searchable
+				/>
+			)
+
+		case questionType.agreement:
+			return (
+				<Checkbox
+					label={question.title}
+					required={question.settings.required}
+					key={form.key(question.key)}
+					{...form.getInputProps(question.key)}
+				/>
+			)
+		case questionType.phone:
+			return (
+				<div key={question.key}>
+					<Text size='sm'>
+						{question.title}{' '}
+						<span className='text-[var(--mantine-color-red-8)]'>
+							{' '}
+							*
+						</span>
+					</Text>
+					<PhoneInput
+						defaultCountry='us'
+						placeholder='Enter your phone number'
+					/>
+				</div>
+			);
+		case questionType.file:
+			return (
+				<>
+					<Group justify="center">
+						<FileButton onChange={setFile} accept="image/png,image/jpeg">
+							{(props) => <Button {...props}>Upload image</Button>}
+						</FileButton>
+					</Group>
+
+					{file && (
+						<Text size="sm" ta="center" mt="sm">
+							Picked file: {file.name}
+						</Text>
+					)}
+				</>
+			)
+		default:
+			return (
+				<h1 key={form.key(question.key)}>Not implemented yet</h1>
+			)
+	}
+}
+
+export default function ViewForm({ params }: { params: { formId: string } }) {
+	const phoneUtil = PhoneNumberUtil.getInstance();
+	const [isValid, setIsValid] = useState(false);
+	const [formSections, setFormSections] = useState<FormSection[]>([]);
+	const [phone, setPhone] = useState('');
+	const userId = 'test-user';
+
+	const form = useForm<Record<string, any>>({
+		mode: 'uncontrolled',
+		initialValues: {},
+	});
+
+	useEffect(() => {
+		getApplication('SWAMPHACKS-X').then((resp) => {
+			console.log(resp);
+			if (!resp || !resp.application?.sections) {
+				return;
+			}
+			const application: Form = resp.application;
+			const sections = resp.application.sections as unknown as FormSection[];
+			setFormSections(sections);
+			const initialValues: Record<string, any> = {};
+			sections.forEach((section: FormSection) => {
+				section.questions.forEach((question: QuestionInterface) => {
+					switch (question.type) {
+						case questionType.checkbox:
+						case questionType.agreement:
+							initialValues[question.key] = false;
+							break;
+						default:
+							initialValues[question.key] = '';
+							break;
+					}
+				});
+			});
+			form.reset();
+			form.setInitialValues({ values: initialValues });
+
+			// getFormResponse(application.id, userId).then((resp: ApplicationResponse) => {
+			// 	const values: Record<string, any> = {};
+			// 	for (const response of resp.general) {
+			// 		values[response.key] = response.value;
+			// 		console.log(response.key, response.value);
+			// 	}
+			// 	for (const response of resp.agreements) {
+			// 		values[response.key] = response.value;
+			// 		console.log(response.key, response.value);
+			// 	}
+			// 	Object.entries(resp.sections).forEach(([key, section]) => {
+			// 		for (const response of section) {
+			// 			values[response.key] = response.value;
+			// 		}
+			// 	});
+			// 	console.log(values);
+			// 	form.setValues(values);
+			// 	form.resetDirty();
+			// });
+		});
+	}, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+	const isPhoneValid = (phone: string) => {
+		try {
+			return phoneUtil.isValidNumber(phoneUtil.parseAndKeepRawInput(phone));
+		} catch (error) {
+			console.log(phone);
+			console.log('not valid phone number');
+			return false;
+		}
+	};
+
+	const handlePhoneChange = (value: string) => {
+		setPhone(value);
+		form.setFieldValue('phoneNumber', value);
+	};
+
+	const handleSubmit = (values: any) => { };
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<div className='flex flex-col items-center'>
+				<div className='m-[5rem] grid w-[30rem] grid-cols-1 gap-4'>
+					{formSections.map((section: FormSection, index: number) => {
+						return (
+							<div key={index}>
+								{/* Title and description */}
+								<Title order={1}>{section.title}</Title>
+								{section.description ? (
+									<Text>{section.description}</Text>
+								) : null}
+								<Divider my="md" />
+
+								{/* Questions */}
+								{section.questions.map((question: QuestionInterface) => {
+									return (
+										<Question key={question.key} question={question} form={form} />
+									)
+								})}
+							</div>
+						)
+					})}
+				</div>
+			</div>
+		</form>
+	);
+}
