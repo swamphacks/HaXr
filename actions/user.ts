09e15811482c91ca2a65864701ca9a0e41c11484
AlexanderWangY <alexander.yisu.wang@outlook.com>
2024-07-31 02:28:11 +0800

    uploading avatar beta

--- actions/user.ts
+++ actions/user.ts
@@ -2,13 +2,13 @@
 
 import prisma from '@/prisma';
 import { type User } from '@prisma/client';
+import { del } from '@vercel/blob'
 
 const updateUserProfile = async (user_id: string, user: User) => {
-  // Check validity here TODO
   try {
     const new_user = await prisma.user.update({
       where: {
-        id: user_id,
+        id: user_id
       },
       data: { ...user },
     });
@@ -19,4 +19,40 @@ const updateUserProfile = async (user_id: string, user: User) => {
   }
 };
 
-export { updateUserProfile };
+const updateUserAvatar = async (user_id: string, user_avatar_url: string) => {
+  try {
+    if (!user_avatar_url) throw new Error("Missing url");
+    const new_user = await prisma.user.update({
+      where: {
+        id: user_id
+      },
+      data: {
+        image: user_avatar_url
+      }
+    })
+
+    return new_user;
+  } catch {
+    return null;
+  }
+}
+
+const deleteUserAvatar = async (user_id: string) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: user_id
+    }
+  })
+
+  if (!user?.image) {
+    return
+  }
+
+  try {
+    await del(user.image);
+  } catch {
+    console.log("Could not delete");
+  }
+}
+
+export { updateUserProfile, updateUserAvatar, deleteUserAvatar };
