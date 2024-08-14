4fbdcd4c30df043a41e311ecec878d431a992e56
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-08-14 16:01:11 -0400

    Refactor form routes and remove modal for form creation

--- app/admin/comp/apps/forms/edit/application/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { CreateApplication } from '@/components/formCreator/FormCreator';
-
-export default function Creator({ params }: { params: { id: string } }) {
-  return <CreateApplication params={params} />;
-}
