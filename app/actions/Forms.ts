1b67b42ad4893821d45a2058202d4ec04cc96e3e
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-08-14 13:15:15 -0400

    Fill checkboxes with autosaved results

--- app/actions/Forms.ts
+++ app/actions/Forms.ts
@@ -9,251 +9,250 @@ import { put, del } from '@vercel/blob';
 import { revalidatePath } from 'next/cache';
 
 const neon = new Pool({
-	connectionString: process.env.POSTGRES_PRISMA_URL,
+  connectionString: process.env.POSTGRES_PRISMA_URL,
 });
 const adapter = new PrismaNeon(neon);
 const prisma = new PrismaClient({ adapter });
 
 export async function getApplication(competition_code: string) {
-	const resp = await prisma.competition.findFirst({
-		where: {
-			code: competition_code,
-			application: {
-				closes_at: {
-					gte: new Date(),
-				},
-				is_published: true,
-			},
-			OR: [
-				{
-					application: {
-						opens_at: null,
-					},
-				},
-				{
-					application: {
-						opens_at: {
-							lte: new Date(),
-						},
-					},
-				},
-			],
-		},
-		include: {
-			application: true,
-		},
-	});
-	return resp;
+  const resp = await prisma.competition.findFirst({
+    where: {
+      code: competition_code,
+      application: {
+        closes_at: {
+          gte: new Date(),
+        },
+        is_published: true,
+      },
+      OR: [
+        {
+          application: {
+            opens_at: null,
+          },
+        },
+        {
+          application: {
+            opens_at: {
+              lte: new Date(),
+            },
+          },
+        },
+      ],
+    },
+    include: {
+      application: true,
+    },
+  });
+  return resp;
 }
 
 function constructFilename(
-	competitionCode: string,
-	formId: string,
-	responseId: string,
-	questionId: string,
-	file: File
+  competitionCode: string,
+  formId: string,
+  responseId: string,
+  questionId: string,
+  file: File
 ) {
-	return `${competitionCode}/forms/${formId}/responses/${responseId}/${questionId}/${file.name}`;
+  return `${competitionCode}/forms/${formId}/responses/${responseId}/${questionId}/${file.name}`;
 }
 
 export async function uploadFile(
-	competitionCode: string,
-	formId: string,
-	responseId: string,
-	questionId: string,
-	formData: FormData
+  competitionCode: string,
+  formId: string,
+  responseId: string,
+  questionId: string,
+  formData: FormData
 ) {
-	const file = formData.get('file') as File;
-	const filename = constructFilename(
-		competitionCode,
-		formId,
-		responseId,
-		questionId,
-		file
-	);
-	const resp = await put(filename, file, { access: 'public' });
-	revalidatePath('/');
-	if (!resp) {
-		return null;
-	}
-	return resp;
+  const file = formData.get('file') as File;
+  const filename = constructFilename(
+    competitionCode,
+    formId,
+    responseId,
+    questionId,
+    file
+  );
+  const resp = await put(filename, file, { access: 'public' });
+  revalidatePath('/');
+  if (!resp) {
+    return null;
+  }
+  return resp;
 }
 
 export async function deleteFile(url: string) {
-	try {
-		await del(url);
-	} catch {
-		// del should throw an error if the url is invalid
-		console.log('Failed to delete file with url: ', url);
-	}
+  try {
+    await del(url);
+  } catch {
+    // del should throw an error if the url is invalid
+    console.log('Failed to delete file with url: ', url);
+  }
 }
 
 export async function getForm(formId: string) {
-	const form = await prisma.form.findFirst({
-		where: {
-			id: formId,
-			// closes_at: {
-			// 	// lte: new Date(),
-			// },
-		},
-	});
-	return form;
+  const form = await prisma.form.findFirst({
+    where: {
+      id: formId,
+      // closes_at: {
+      // 	// lte: new Date(),
+      // },
+    },
+  });
+  return form;
 }
 
 export async function createForm(competitionCode: string, formType: FormType) {
-	return await prisma.form.create({
-		data: {
-			competition_code: competitionCode,
-			form_type: formType,
-		},
-	});
+  return await prisma.form.create({
+    data: {
+      competition_code: competitionCode,
+      form_type: formType,
+    },
+  });
 }
 
 export async function updateForm(form: Form, sections: FormSection[]) {
-	// ensure the form is not published
-	const resp = await prisma.form.update({
-		where: {
-			id: form.id,
-			is_published: false,
-		},
-		data: {
-			...form,
-			sections: sections as unknown as Prisma.JsonArray,
-		},
-	});
+  // ensure the form is not published
+  const resp = await prisma.form.update({
+    where: {
+      id: form.id,
+      is_published: false,
+    },
+    data: {
+      ...form,
+      sections: sections as unknown as Prisma.JsonArray,
+    },
+  });
 
-	return resp;
+  return resp;
 }
 
 export async function updateFormSettings(form: Form) {
-	// remove sections and is_mlh from the form object
-	// we don't want to edit questions because the form may be published
-	const clone = (({ id, sections, is_mlh, ...rest }) => rest)(form);
+  // remove sections and is_mlh from the form object
+  // we don't want to edit questions because the form may be published
+  const clone = (({ id, sections, is_mlh, ...rest }) => rest)(form);
 
-	const resp = await prisma.form.update({
-		where: {
-			id: form.id,
-		},
-		data: {
-			...clone,
-		},
-	});
+  const resp = await prisma.form.update({
+    where: {
+      id: form.id,
+    },
+    data: {
+      ...clone,
+    },
+  });
 
-	return resp;
+  return resp;
 }
 
 export async function saveAndPublishForm(
-	form: Form,
-	sections: FormSection[],
-	settings: FormSettings
+  form: Form,
+  sections: FormSection[],
+  settings: FormSettings
 ) {
-	const resp = await prisma.form.update({
-		where: {
-			id: form.id,
-		},
-		data: {
-			...form,
-			...settings,
-			sections: sections as unknown as Prisma.JsonArray,
-			is_published: true,
-		},
-	});
+  const resp = await prisma.form.update({
+    where: {
+      id: form.id,
+    },
+    data: {
+      ...form,
+      ...settings,
+      sections: sections as unknown as Prisma.JsonArray,
+      is_published: true,
+    },
+  });
 
-	return resp;
+  return resp;
 }
 
 export async function publishForm(form: Form, sections: FormSection[]) {
-	const resp = await prisma.form.update({
-		where: {
-			id: form.id,
-			is_published: false,
-		},
-		data: {
-			...form,
-			sections: sections as unknown as Prisma.JsonArray,
-			is_published: true,
-		},
-	});
+  const resp = await prisma.form.update({
+    where: {
+      id: form.id,
+      is_published: false,
+    },
+    data: {
+      ...form,
+      sections: sections as unknown as Prisma.JsonArray,
+      is_published: true,
+    },
+  });
 
-	return resp;
+  return resp;
 }
 
 export async function createResponse(
-	formId: string,
-	userId: string,
-	answers: Record<string, string>
+  formId: string,
+  userId: string,
+  answers: Record<string, string>
 ) {
-	const resp = await prisma.response.create({
-		data: {
-			form_id: formId,
-			submitted_by_id: userId,
-			answers: answers,
-		},
-	});
+  const resp = await prisma.response.create({
+    data: {
+      form_id: formId,
+      submitted_by_id: userId,
+      answers: answers,
+    },
+  });
 
-	return resp;
+  return resp;
 }
 
 export async function getFormResponse(
-	formId: string,
-	userId: string,
-	initialValues: React.MutableRefObject<Record<string, any>>
+  formId: string,
+  userId: string,
+  initialValues: React.MutableRefObject<Record<string, any>>
 ) {
-	let resp = await prisma.response.findFirst({
-		where: {
-			form_id: formId,
-			submitted_by_id: userId,
-		},
-	});
+  let resp = await prisma.response.findFirst({
+    where: {
+      form_id: formId,
+      submitted_by_id: userId,
+    },
+  });
 
-	// Create a new response if it doesn't exist
-	if (!resp) {
-		resp = await prisma.response.create({
-			data: {
-				submitted_by_id: userId,
-				form_id: formId,
-				answers: initialValues.current,
-			},
-		});
-	}
-	return resp;
+  // Create a new response if it doesn't exist
+  if (!resp) {
+    resp = await prisma.response.create({
+      data: {
+        submitted_by_id: userId,
+        form_id: formId,
+        answers: initialValues.current,
+      },
+    });
+  }
+  return resp;
 }
 
 export async function saveResponse(
-	responseId: string,
-	responses: Record<string, string>
+  responseId: string,
+  responses: Record<string, string>
 ) {
-	const resp = await prisma.response.update({
-		where: {
-			id: responseId,
-			submitted: false,
-		},
-		data: {
-			answers: responses,
-		},
-	});
+  const resp = await prisma.response.update({
+    where: {
+      id: responseId,
+      submitted: false,
+    },
+    data: {
+      answers: responses,
+    },
+  });
 
-	return resp;
+  return resp;
 }
 
 export async function submitResponse(
-	responseId: string,
-	responses: Record<string, string>
+  responseId: string,
+  responses: Record<string, string>
 ) {
-	try {
-		const resp = await prisma.response.update({
-			where: {
-				id: responseId,
-				submitted: false,
-			},
-			data: {
-				answers: responses,
-				submitted: true,
-			},
-		});
-		return resp;
-	} catch {
-		return null;
-	}
-
+  try {
+    const resp = await prisma.response.update({
+      where: {
+        id: responseId,
+        submitted: false,
+      },
+      data: {
+        answers: responses,
+        submitted: true,
+      },
+    });
+    return resp;
+  } catch {
+    return null;
+  }
 }
