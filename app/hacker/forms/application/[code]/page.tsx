7b9b3d5037c03638b26473914c8d61959679d896
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-08-24 21:57:42 -0400

    connect user to form submission

--- app/hacker/forms/application/[code]/page.tsx
+++ app/hacker/forms/application/[code]/page.tsx
@@ -1,5 +1,9 @@
 import ViewForm from '@/components/forms/Form';
+import { Session } from 'next-auth';
+import { auth } from '@/auth';
 
-export default function FormView({ params }: { params: { id: string } }) {
-  return <ViewForm formId={params.id} />;
+export default async function FormView({ params }: { params: { id: string } }) {
+
+  const session = await auth();
+  return <ViewForm formId={params.id} session={session!}/>;
 }
--- components/UserAvatar.tsx
+++ components/UserAvatar.tsx
@@ -1,52 +1,53 @@
 import { Session } from 'next-auth';
 import {
-  Avatar,
-  Group,
-  Menu,
-  MenuDropdown,
-  MenuItem,
-  MenuTarget,
-  Stack,
-  Text,
+	Avatar,
+	Group,
+	Menu,
+	MenuDropdown,
+	MenuItem,
+	MenuTarget,
+	Stack,
+	Text,
 } from '@mantine/core';
 import { IconLogout } from '@tabler/icons-react';
 import { useRouter } from 'next/navigation';
 
 interface Props {
-  session: Session;
+	session: Session;
 }
 
 export default function UserAvatar({ session }: Readonly<Props>) {
-  const router = useRouter();
+	const router = useRouter();
 
-  if (!session?.user?.image || !session.user.name || !session.user.email)
-    return null;
-  const { image, name, email } = session.user;
+	console.log(session);
+	if (!session?.user?.image || !session.user.name || !session.user.email)
+		return null;
+	const { image, name, email } = session.user;
 
-  return (
-    <Group justify='center'>
-      <Menu trigger='hover' shadow='md' withArrow>
-        <MenuTarget>
-          <Avatar src={image} radius='xl' />
-        </MenuTarget>
-        <MenuDropdown>
-          <MenuItem
-            color='red'
-            leftSection={<IconLogout />}
-            onClick={() => router.push('/api/auth/signout')}
-          >
-            Sign-out
-          </MenuItem>
-        </MenuDropdown>
-      </Menu>
-      <Stack gap={5}>
-        <Text size='sm' fw={700} style={{ lineHeight: 1 }}>
-          {name}
-        </Text>
-        <Text c='dimmed' size='xs' style={{ lineHeight: 1 }}>
-          {email}
-        </Text>
-      </Stack>
-    </Group>
-  );
+	return (
+		<Group justify='center'>
+			<Menu trigger='hover' shadow='md' withArrow>
+				<MenuTarget>
+					<Avatar src={image} radius='xl' />
+				</MenuTarget>
+				<MenuDropdown>
+					<MenuItem
+						color='red'
+						leftSection={<IconLogout />}
+						onClick={() => router.push('/api/auth/signout')}
+					>
+						Sign-out
+					</MenuItem>
+				</MenuDropdown>
+			</Menu>
+			<Stack gap={5}>
+				<Text size='sm' fw={700} style={{ lineHeight: 1 }}>
+					{name}
+				</Text>
+				<Text c='dimmed' size='xs' style={{ lineHeight: 1 }}>
+					{email}
+				</Text>
+			</Stack>
+		</Group>
+	);
 }
