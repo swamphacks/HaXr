d356fc902420d3e4c811fa396c71064d75869f79
Alexander Wang <alexander.yisu.wang@outlook.com>
2024-11-13 09:27:51 -0500

    added type for forms

--- global.d.ts
+++ global.d.ts
@@ -14,3 +14,27 @@ export declare module '@auth/core/types' {
     resumeUrl: string | null;
   }
 }
+
+export declare module '@forms/core/types' {
+  interface Form2024 {
+    firstName: string;
+    lastName: string;
+    email: string;
+    phoneNumber: string;
+    age: string;
+    certAge: boolean;
+    school: string;
+    levelOfStudy: string;
+    major: string;
+    graduationMonth: string;
+    graduationYear: string;
+    hackathonExperience: string;
+    teamStatus: string;
+    tshirtSize: string;
+    dietaryRestrictions: string[];
+    referralSource: string[];
+    photoConsent: boolean;
+    inPersonConsent: boolean;
+    codeOfConductConsent: boolean;
+  }
+}
