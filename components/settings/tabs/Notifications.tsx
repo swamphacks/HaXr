e50628f191410b8f88695b1246b20118de614199
Robert Conde <rconde2003@gmail.com>
2024-11-18 01:32:08 -0500

    Fix casing

--- components/settings/tabs/Notifications.tsx
+++ components/settings/tabs/Notifications.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Notifications() {
   return (
     <Stack w='100%' h='100%' pr={20} pl={20}>
-      <Title order={2}>notifications</Title>
+      <Title order={2}>Notifications</Title>
       <Divider />
       <Text>Nothing to see here yet... stay tuned!</Text>
     </Stack>
