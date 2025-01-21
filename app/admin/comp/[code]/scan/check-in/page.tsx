ae9c9767be5311cacbf17e94bee539b3c54c6b59
Robert Conde <rconde2003@gmail.com>
2025-01-21 01:14:00 -0500

    Oops

--- app/admin/comp/[code]/scan/check-in/page.tsx
+++ app/admin/comp/[code]/scan/check-in/page.tsx
@@ -1,8 +1,6 @@
 import CheckIn from '@/components/checkin/CheckIn';
 import { getCheckInApplicants } from '@/actions/scanning';
 import { TypedApplication } from '@/app/hacker/application/[code]/page';
-import { getCheckInApplicants } from '@/actions/scanning';
-import { TypedApplication } from '@/app/hacker/application/[code]/page';
 
 interface Props {
   params: {
@@ -12,7 +10,7 @@ interface Props {
 
 export default async function ScanCheckIn({ params: { code } }: Props) {
   const checkInApplicants = (await getCheckInApplicants(code)).map(
-    ({ content, attendee, ...rest }) => ({
+    ({ content, ...rest }) => ({
       ...rest,
       content: content as unknown as TypedApplication['content'],
     })
